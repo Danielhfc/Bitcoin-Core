26436dce049d4215a112a1147b624c0435dbc8a6
laanwj <126646+laanwj@users.noreply.github.com>
2024-08-27 18:47:06 +0200

    fixup: mapport.cpp: replace GetRandMillis, it doesn't exist anymore

--- src/mapport.cpp
+++ src/mapport.cpp
@@ -125,7 +125,7 @@ static bool ProcessPCP()
         // distribution in the range 1/2 to 5/8 of expiration time.
         std::chrono::seconds sleep_time_min(actual_lifetime / 2);
         std::chrono::seconds sleep_time_max(actual_lifetime * 5 / 8);
-        sleep_time = sleep_time_min + GetRandMillis(sleep_time_max - sleep_time_min);
+        sleep_time = sleep_time_min + FastRandomContext().randrange<std::chrono::milliseconds>(sleep_time_max - sleep_time_min);
     } while (ret && g_mapport_interrupt.sleep_for(sleep_time));
 
     // We don't delete the mappings when the thread is interrupted because this would add additional complexity, so
